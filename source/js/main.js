44bcbb818921b84b2ca51e3b85ab9b869ce89bce
mixailyo <67204306+mixailyo@users.noreply.github.com>
2022-04-08 16:04:56 +0300

    update

--- source/js/main.js
+++ source/js/main.js
@@ -24,3 +24,42 @@ teamsItemTitle.forEach((title, i) => {
     });
   });
 });
+
+// Аккордеон в вакнсиях
+
+let subdivisionVacancyTitle = document.querySelectorAll(".subdivision__vacancy-title");
+
+subdivisionVacancyTitle.forEach((title, i) => {
+  title.addEventListener("click", () => {
+    title.closest(".subdivision__vacancy").classList.toggle("subdivision__vacancy--active");
+    let description = title
+      .closest(".subdivision__vacancy")
+      .querySelector(".subdivision__vacancy-more");
+    if (description.style.maxHeight) {
+      description.style.maxHeight = null;
+    } else {
+      description.style.maxHeight = description.scrollHeight + "px";
+    }
+  });
+});
+
+// Слайдер в about vk teams
+if (document.documentElement.clientWidth < 768) {
+  let aboutVkteamsCardsListWrapper = document.querySelector('.about-vkteams__cards-list-wrapper');
+  let aboutVkteamsCard = aboutVkteamsCardsListWrapper.querySelectorAll('.about-vkteams__card');
+
+  if (aboutVkteamsCardsListWrapper) {
+    aboutVkteamsCard.forEach(card => {
+      card.classList.add('swiper-slide')
+    })
+
+    const swiper = new Swiper(aboutVkteamsCardsListWrapper, {
+      wrapperClass: 'about-vkteams__cards-list',
+      slidesPerView: 1.25,
+    
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
+  }
+}
